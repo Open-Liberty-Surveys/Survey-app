88c9fc2cdc4f83322bd16ebe34629874ed129679
ahayes3 <48255118+ahayes3@users.noreply.github.com>
2019-12-04 00:07:47 -0500

    ABLE TO MERG! (#18)
    * fix
    
    * css updates
    
    * fixed bugs with surveys and updated other gui elements
    
    * rest update
    
    * rest update
    
    * Trying to work

--- SurveyService/src/main/java/survey/PropertiesResource.java
+++ SurveyService/src/main/java/survey/PropertiesResource.java
@@ -1,25 +1,44 @@
 package survey;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Properties;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import java.io.File;
+import java.io.*;
 import javax.ws.rs.core.MediaType;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonObject;
+import javax.json.Json;
+import javax.ws.rs.core.Response;
 
-// tag::path[]
-@Path("properties")
-// end::path[]
+@Path("surveys")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class PropertiesResource {
 
-    // tag::get[]
     @GET
-    // end::get[]
-    // tag::produces[]
-    @Produces(MediaType.APPLICATION_JSON)
-    // end::produces[]
-    public Properties getProperties() {
-        return System.getProperties();
+    public Response getSurveys() {
+	    System.out.println("CALLED");
+            JsonObjectBuilder survey=Json.createObjectBuilder();
+	    survey.add("user","Andy");
+	    survey.add("question", "Beans or no?");
+	    JsonObject built=survey.build();
+	    return Response.ok(built,MediaType.APPLICATION_JSON).build();
     }
 
+    @POST
+    public String addSurvey(String surveyJSON) { return "test1"; }
+
+    @Path("/{user}")
+    @GET
+    public String getUserSurveys(@PathParam("user") final String user) {
+        return user + "'s surveys";
+    }
+
+    @Path("/{user}/{key}")
+    @GET
+    public String getSearchSurveys(@PathParam("user") final String user, @PathParam("key") final String key) {
+        return user + "'s surveys with " + key;
+    }
 }
